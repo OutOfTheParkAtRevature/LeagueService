29509755d873b8c472288c29b587ac7311784a5b
Joshua Raneri <raneri.josh@gmail.com>
2021-02-05 15:06:49 -0500

    first commit

---
+++ Repo/Repo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Repo
+{
+    public class Repo
+    {
+        /// <summary>
+        /// Get user Role
+        /// </summary>
+        /// <param name="id">UserID</param>
+        /// <returns>RoleID</returns>
+        public async Task<Role> GetRoleById(int id)
+        {
+            return await _repo.GetRoleById(id);
+        }
+        /// <summary>
+        /// Get list of user Roles
+        /// </summary>
+        /// <returns>list of Roles</returns>
+        public async Task<IEnumerable<Role>> GetRoles()
+        {
+            return await _repo.GetRoles();
+        }
+        /// <summary>
+        /// Edit User to change Role
+        /// </summary>
+        /// <param name="userId">UserID</param>
+        /// <param name="roleId">RoleID</param>
+        /// <returns>Role added</returns>
+        public async Task<Role> EditUserRole(Guid userId, int roleId)
+        {
+            User tUser = await GetUserById(userId);
+            tUser.RoleID = roleId;
+            await _repo.CommitSave();
+            return await GetRoleById(roleId);
+        }
+    }
+}
